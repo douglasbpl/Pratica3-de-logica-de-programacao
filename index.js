73e248976907b2c9b9b82c6d81f06a94b26454a3
douglasbpl <douglas.bpl94@yahoo.com.br>
2021-05-14 19:09:20 -0300

    prática 3 de lógica de programação

---
+++ index.js
@@ -0,0 +1,34 @@
+//  contagem de 1 até 10
+
+for(let cre = 1; cre <= 10; cre ++)
+ {console.log(cre)
+}
+
+//  contagem de 10 até 1
+
+
+let decre = 10
+
+while(decre >= 1)
+{console.log(decre)
+ decre -- 
+}
+
+// numeros impares de 1 a 100
+
+let impar = 1
+
+while(impar <= 100)
+{console.log(impar)
+impar +=2
+}
+
+
+// numeros pares de 0 a 100
+
+for(let par = 0; par <= 100; par +=2 )
+{console.log(par)
+}
+
+
+
